d350a3fc9a5ab3ac486338613ced888011b09ac9
lindagez <lidiyagez@gmail.com>
2023-07-04 13:05:36 -0700

    Data Manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+fuction getListStudents(){
+return
+[
+
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+]}
